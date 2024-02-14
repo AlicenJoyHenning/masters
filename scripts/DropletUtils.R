607fcac919022427ba0cbb17510696ed1f1af0cd
Alicen Henning <129797527+AlicenJoyHenning@users.noreply.github.com>
2024-02-14 14:11:51 +0200

    edit

--- scripts/DropletUtils.R
+++ scripts/DropletUtils.R
@@ -1,6 +1,11 @@
 # Installing and looking at documentation for DropletUtils
 
+# DropletUtils 
 if (!require("BiocManager", quietly = TRUE))
   install.packages("BiocManager")
 
 BiocManager::install("DropletUtils")
+# took a while
+
+browseVignettes("DropletUtils")
+
