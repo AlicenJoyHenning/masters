8e63674fe184e0067fc2dedeedda1c6a72028564
Alicen Henning <129797527+AlicenJoyHenning@users.noreply.github.com>
2024-02-28 08:47:57 +0200

    Update monocle3.R

--- scripts/monocle3.R
+++ scripts/monocle3.R
@@ -1,4 +1,17 @@
 #### MONOCLE3 TESTING
 # 27/02/2024
-# This R script explores the Monocle 3 vignette using the interferon dataset I have installed 
+# This R script explores the Monocle 3 vignette using the interferon dataset 
 
+# [0] Installing the Biocoductor dependencies & monocle3 ####
+
+BiocManager::install(c('BiocGenerics', 'DelayedArray', 'DelayedMatrixStats',
+'limma', 'lme4', 'S4Vectors', 'SingleCellExperiment',
+'SummarizedExperiment', 'batchelor', 'HDF5Array',
+'terra', 'ggrastr', 'devtools'))
+
+install.packages("devtools")
+devtools::install_github('cole-trapnell-lab/monocle3')
+
+
+
+# [1] 
