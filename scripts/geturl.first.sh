3293d11af1d3b19b1afd08e589278a7a7889d890
Alicen Henning <129797527+AlicenJoyHenning@users.noreply.github.com>
2024-03-15 15:35:30 +0200

    New script to ask for help on other computer

--- scripts/geturl.first.sh
+++ scripts/geturl.first.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Define the list of accession numbers
-Accession_list=("GSM7135569" "GSM7135585" "GSM7103257" "GSM7103264" "GSM7103275" "GSM7103276" "GSM7103277" "GSM7103278" "GSM7103317" "GSM7103319" "GSM7103320" "GSM7103327")
+Accession_list=("GSM5747736","GSM5747737","GSM5747738","GSM5747739","GSM5747740","GSM5747741","GSM5747742","GSM5747743","GSM5747744","GSM5747745","GSM5747746")
 
 # Iterate through the list
 for accession in "${Accession_list[@]}"; do
@@ -15,9 +15,7 @@ for accession in "${Accession_list[@]}"; do
     grep -v "null" "$accession.temp.txt" > "$accession.metadata.txt"
     
     # Count the number of fastq files in each file
-    echo "$accession" > fastq.count.txt
-    echo "," <redirection-command-for-same-line> fastq.count.txt
-    grep -c "ftp" "$accession.metadata.txt" <redirection-command-for-same-line> fastq.count.txt
+    grep -c "ftp" "$accession.metadata.txt" >> fastq.count.txt
 
     # Remove temporary file 
     rm "$accession.txt" "$accession.temp.txt"
