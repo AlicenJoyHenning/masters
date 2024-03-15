3bf9bce3fa33dbfa8a863c4846b911634267b35a
Alicen Henning <129797527+AlicenJoyHenning@users.noreply.github.com>
2024-03-15 14:36:32 +0200

    Adding geturl script

--- scripts/geturl.sh
+++ scripts/geturl.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
-acc_numbers=
+# Define the list of accession numbers
+Accession_list=("GSM5747736","GSM5747737","GSM5747738","GSM5747739","GSM5747740","GSM5747741","GSM5747742","GSM5747743","GSM5747744","GSM5747745","GSM5747746")
 
+# Iterate through the list
+for accession in "${Accession_list[@]}"; do
+    # Create a file with the accession number
+    echo "Creating file for accession $accession"
+    touch "$accession.txt"
 
+    # Execute commands on the created file
+    ffq "$accession" > "$accession.txt"
+    grep "md5\|fastq.gz" "$accession.txt" > "$accession.temp.txt"
+    grep -v "null" "$accession.temp.txt" > "$accession.metadata.txt"
+
+    # Remove temporary files
+    rm "$accession.txt" "$accession.temp.txt"
+done
